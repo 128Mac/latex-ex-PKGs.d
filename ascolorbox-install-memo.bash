11370301a605cf29e0ebb2783ea9371f87a08799
128Mac <qaz_wada136@yahoo.co.jp>
2022-12-12 12:26:45 +0900

    fix symbolic link 対応と設置確認メッセージ

--- ascolorbox-install-memo.bash
+++ ascolorbox-install-memo.bash
@@ -10,6 +10,11 @@
    texmfhome=$(kpsewhich --var-value TEXMFHOME)
 
    [ -d "$texmfhome/tex"     ] || mkdir  $texmfhome/tex
-   [ -e "$texmfhome/tex/$t3" ] && rm -rf $texmfhome/tex/$t3
+   [ -e "$texmfhome/tex/$t3" ] && rm -r  $texmfhome/tex/$t3
+   [ -L "$texmfhome/tex/$t3" ] && rm     $texmfhome/tex/$t3
    ln -s                      $(pwd)/$t3 $texmfhome/tex/$t3
+
+   echo
+   echo kpsewhich -all ascolorbox.sty の結果は以下の通り
+   kpsewhich -all ascolorbox.sty | /usr/bin/perl -pne 's%'$HOME'%~%'
 }
