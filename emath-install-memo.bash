73734410384bceef257030db4b03022ac05fcb46
128Mac <qaz_wada136@yahoo.co.jp>
2022-12-15 12:25:11 +0900

    Update emath-install-memo.bash
    zsh アドホック対策

--- emath-install-memo.bash
+++ emath-install-memo.bash
@@ -61,6 +61,7 @@
     echo Download URL: $URL
 
     t4=$(find $t3 -iname '*.sty'| xargs -L 1 basename | sort | head)
+    t4=emath.sty
     echo kpsewhich -all $t4 の結果は以下の通り
     kpsewhich -all $t4 | perl -pne 's%'$HOME'%~%'
 
