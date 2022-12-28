e55677c7b77b2da75ddb61f11e1d77b6b6f4e669
128Mac <qaz_wada136@yahoo.co.jp>
2022-12-29 01:06:26 +0900

    fix 丸ごとパック及び訂正版の情報をディレクトリ名に組み込んだ
    - emath の丸ごとパックと訂正版の情報をトップディレクトリのファイル名に設定
    - 全体的には zip 展開方式を統一
       - 大元は $ARC から
       - 丸ごとパックは三つに分かれているので $emath に一旦展開し、三つをそれぞれ #emath で展開
       - 展開した zip ファイルは削除しておく
    - git を利用し丸ごとパックを　main branch に登録し 訂正版を その zip ファイル名由来のブランチに登録

--- emath-install-memo.bash
+++ emath-install-memo.bash
@@ -1,54 +1,87 @@
 #!/bin/bash
 
-{
+: main && {
     ARC=zzARCHIVES
-    USR="emath:$(date +%Y)"
+
+    [ -d "$ARC" ] && find $ARC -delete
+    [ -d "$ARC" ] || mkdir -p $ARC
 
     URL=http://emath.s40.xrea.com/allinone.htm
-    DLURL=http://emath.la.coocan.jp/sty
-
-    # 丸ごとパック
-    zipfile=$(
-        curl --silent --location $URL |
-            grep 'emath.s40.xrea.com.*zip' |
-            tr '<">' '\n'|
-            grep zip
-           )
-
-    t3=$(basename $zipfile .zip)
-
-    [ -d $ARC  ] || mkdir -p $ARC
-    [ -d $t3   ] && rm -rf $t3
-
-    mkdir $t3
-
-    #cd $t3
-
-    curl --silent --location  --output $ARC/$zipfile --remote-time $DLURL/$zipfile
-
-    unzip  -q -o -d $ARC $ARC/$zipfile -x readme.txt
-
-    zipinfo -1 $ARC/$zipfile -x readme.txt |
-        while read zip ; do
-            unzip -q -o -d $t3 $ARC/$zip
-            rm $ARC/$zip
-        done
-    rm $ARC/$zipfile
-
-    # 訂正版
     URLTEISEI=http://emath.s40.xrea.com/teisei.htm
 
-    curl --silent --location $URLTEISEI |
-        iconv -f sjis -t utf8 |
-        sed '/<!---/,/--->/d;/----------/,$d' |
-        tr '<"/>( )' '\n' |
-        grep zip |
-        uniq |
-        while read zipfile ; do
-            curl --silent --location --output $ARC/$zipfile $DLURL/$zipfile
-            unzip  -q -o -d $t3 $ARC/$zipfile
-            rm $ARC/$zipfile
+    infoINFO=$(
+        for url in $URL $URLTEISEI ; do
+            curl --silent --location $url |
+                iconv -f sjis -t utf8 |
+                perl -ne '
+                    exit if ( /旧訂正版/ );
+                    print "$2 $1\n"    if ( /.* href="([^"]+)["]>([^<>]+[.]zip)<.*/   ) ;# 丸ごとパック/訂正
+                    print "$2 $1/$2\n" if ( /.* href="([^"]+)[\/]([^"]+[0-9][.]zip)"/ ) ;# perl
+                '
+        done |
+            sort -u
+            )
+
+    function mySel() {
+        key=$1 ; shift
+        val=$1 ; shift
+        echo $@ |
+            awk '
+            {
+                for ( i = 1 ; i < NF ; i += 2 ) {
+                    if ( $(i+1) ~ /'$key'/  ) {
+                        print $(i+'$val');
+                        exit
+                    }
+                }
+            }
+            '
+    }
+
+    marugotoZIP=$( mySel 'lime.cgi\?0001'   0 $infoINFO )
+    marugotoURL=$( mySel 'lime.cgi\?0001'   1 $infoINFO )
+
+    teiseiZIP=$(   mySel 'lime.cgi\?teisei' 0 $infoINFO )
+    teiseiURL=$(   mySel 'lime.cgi\?teisei' 1 $infoINFO )
+
+    perlZIP=$(     mySel 'emathpl'          0 $infoINFO )
+    perlURL=$(     mySel 'emathpl'          1 $infoINFO )
+
+    curl --silent --location --output $ARC/$marugotoZIP $marugotoURL
+    curl --silent --location --output $ARC/$teiseiZIP   $teiseiURL
+    curl --silent --location --output $ARC/$perlZIP     $perlURL
+
+    emath=$(basename $marugotoZIP .zip)-$(basename $teiseiZIP .zip)
+    [ -d "$emath" ] && find $emath -delete
+    mkdir -p $emath
+
+    # 丸ごとパック展開
+    unzip -q -d $emath $ARC/$marugotoZIP && rm  $ARC/$marugotoZIP
+    find $emath -iname '*.zip' |
+        while read zip ; do
+            unzip -q -d $emath $zip
+            rm $zip
         done
+    git -C $emath init --quiet
+    git -C $emath add .
+    git -C $emath commit --quiet -am "$(basename $marugotoZIP .zip) from $marugotoURL"
+
+    # 訂正版で置き換え
+    # ブランチを切っておく
+    git -C $emath checkout --quiet -b $(basename $teiseiZIP .zip)
+    unzip -q -o -d $emath $ARC/$teiseiZIP && rm $ARC/$teiseiZIP
+    git -C $emath add $(
+        git -C $emath status |
+            awk -v RS= '/Untracked/ { gsub (/.*\)/, ""); print}'
+        )
+    git -C $emath commit --quiet -am "$(basename $teiseiZIP .zip) from $teiseiURL"
+
+    perl=$(basename $perlURL .zip)
+    [ -d "$perl" ] && find $perl -delete
+    mkdir -p $perl
+    unzip -q -d $perl $ARC/$perlZIP && rm  $ARC/$perlZIP
+
+    t3=$emath
 
     texmfhome=$(kpsewhich --var-value TEXMFHOME)
 
@@ -58,15 +91,16 @@
     ln -s                      $(pwd)/$t3 $texmfhome/tex/$t3
 
     echo
-    echo Download URL: $URL
+    echo "Download URL: $URL"
+    echo "              $URLTEISEI"
 
     t4=$(find $t3 -iname '*.sty'| xargs -L 1 basename | sort | head)
-    t4=emath.sty
-    echo kpsewhich -all $t4 の結果は以下の通り
+    t4=$(echo emath.sty $t4)
+    echo kpsewhich -all emath.sty $t4 の結果は以下の通り
     kpsewhich -all $t4 | perl -pne 's%'$HOME'%~%'
 
     perl5lib=$(
-        find $(pwd)/$3 -iname '*.pl' -exec dirname {} \; |
+        find $(pwd)/$(basename $perlZIP .zip) -iname '*.pl' -exec dirname {} \; |
             sort -u |
             perl -pne 's%'$HOME'%~%' |
             xargs | tr ' ' ':')
